887dede9358881e8bc12c881184d7484d9dad18a
Flânelon DJorge Silva Salazar <flanelon18@gmail.com>
2024-01-13 15:30:06 -0300

    add user model and add server rotes: get and post auth

--- app.js
+++ app.js
@@ -1,8 +1,64 @@
 const express = require("express");
+const mongoose = require("mongoose");
+const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
+const UserModel = require("./models/index");
 
-const app = express();
 require("dotenv").config();
 
-app.listen(process.env.PORT, () => {
-  console.log(`server is running, PORT: ${process.env.PORT}`);
+const app = express();
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.status(200).json({
+    message: "is connected",
+  });
 });
+
+app.post("/auth/register", async (req, res, next) => {
+  const { name, email, password, confirmPassword } = req.body;
+
+  if (!name) {
+    return res.status(422).json({
+      error: "name is required",
+    });
+  }
+
+  if (!email) {
+    return res.status(422).json({
+      error: "email is required",
+    });
+  }
+
+  if (!password) {
+    return res.status(422).json({
+      error: "password is required",
+    });
+  }
+
+  if (!confirmPassword) {
+    return res.status(422).json({
+      error: "confirmPassword is required",
+    });
+  }
+
+  if (password !== confirmPassword) {
+    return res.status(422).json({
+      error: "password and confirmPassword should match.",
+    });
+  }
+
+  return res.status(200).json({
+    msg: "user created successfully",
+  });
+});
+
+mongoose
+  .connect(process.env.URI)
+  .then(() => {
+    app.listen(process.env.PORT);
+    console.log("database is connected, PORT: ", process.env.PORT);
+  })
+  .catch((error) => {
+    console.log(error);
+  });
---
+++ models/index.js
@@ -0,0 +1,19 @@
+const mongoose = require("mongoose");
+require("dotenv").config();
+
+const UserModel = mongoose.model("Users", {
+  name: {
+    type: String,
+    require: true,
+  },
+  email: {
+    type: String,
+    require: true,
+  },
+  password: {
+    type: String,
+    require: true,
+  },
+});
+
+module.exports = UserModel;
